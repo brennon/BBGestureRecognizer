aad0b6239a51c9ecb6202f978783159fc8ba5277
Brennon Bortz <brennon@brennonbortz.com>
2014-12-29 21:27:23 -0500

    Add additional functionality to BBUIGestureREcognizer

--- BBGestureRecognizer/BBUIGestureRecognizer.swift
+++ BBGestureRecognizer/BBUIGestureRecognizer.swift
@@ -33,15 +33,191 @@ enum BBUIGestureRecognizerState: Int, Printable {
     case Failed
     case Recognized
     
-    // called before touchesBegan:withEvent: is called on the gesture recognizer for a new touch. return NO to prevent the gesture recognizer from seeing this touch
-    func gestureRecognizer(gestureRecognizer: BBUIGestureRecognizer, shouldReceiveTouch touch: UITouch) -> Bool
+    var description: String {
+        switch(self) {
+        case Possible:
+            return "Possible"
+        case Began:
+            return "Began"
+        case Changed:
+            return "Changed"
+        case Ended:
+            return "Ended"
+        case Cancelled:
+            return "Cancelled"
+        case Failed:
+            return "Failed"
+        case Recognized:
+            return "Recognized"
+        }
+    }
 }
 
+struct BBUIGestureRecognizerStateTransition {
+    let fromState: BBUIGestureRecognizerState
+    let toState: BBUIGestureRecognizerState
+    let shouldNotify: Bool
+}
+
+/**
+    `BBUIGestureRecognizer` is a base class for concrete gesture-recognizer 
+    classes that can be attached to `BBSKNode` instances. A gesture-recognizer 
+    object--or, simply, a gesture recognizer--decouples the logic for
+    recognizing a gesture and acting on that recognition. When one of these 
+    objects recognizes a common gesture or, in some cases, a change in the 
+    gesture, it sends an action message to each designated target object.
+    <br /><br />
+    The `BBUIGestureRecognizer` class defines a set of common behaviors that can 
+    be configured for subclassed gesture recognizers. It can also communicate 
+    with its delegate (an object that conforms to the 
+    `BBUIGestureRecognizerDelegate` protocol), thereby enabling finer-grained 
+    customization of some behaviors.
+    <br /><br />
+    A gesture recognizer operates on touches hit-tested to a specific `BBSKNode` 
+    and all of that node's descendant nodes. It thus must be associated with 
+    that view. To make that association you must call the `BBSKNode` method
+    `addGestureRecognizer:`.
+    <br /><br />
+    A gesture recognizer has a method associated with it. Recognition of a 
+    gesture results in calling this method. The action method must conform to 
+    the signature: `(BBUIGestureRecognizer?) -> ()`
+    <br /><br />
+    Beyond this, a `BBUIGestureRecognizer` operates much like a 
+    `UIGestureRecognizer`. See the documentation for `UIGestureRecognizer` for 
+    further information.
+*/
 class BBUIGestureRecognizer: Equatable, Printable {
-    private(set) var state: BBUIGestureRecognizerState = .BBUIGestureRecognizerStatePossible
+    
+    /**
+        Only certain transitions from one `BBUIGestureRecognizerState` to
+        another are allowed. `allowedStateTransitions` is an 
+        `[BBUIGestureRecognizerStateTransition]` that includes those transitions 
+        that are allowed.
+    */
+    private let allowedStateTransitions: [BBUIGestureRecognizerStateTransition] = [
+        BBUIGestureRecognizerStateTransition(
+            fromState: .Possible,
+            toState: .Recognized,
+            shouldNotify: true
+        ),
+        BBUIGestureRecognizerStateTransition(
+            fromState: .Possible,
+            toState: .Failed,
+            shouldNotify: false
+        ),
+        BBUIGestureRecognizerStateTransition(
+            fromState: .Possible,
+            toState: .Began,
+            shouldNotify: true
+        ),
+        BBUIGestureRecognizerStateTransition(
+            fromState: .Began,
+            toState: .Changed,
+            shouldNotify: true
+        ),
+        BBUIGestureRecognizerStateTransition(
+            fromState: .Began,
+            toState: .Cancelled,
+            shouldNotify: true
+        ),
+        BBUIGestureRecognizerStateTransition(
+            fromState: .Began,
+            toState: .Ended,
+            shouldNotify: true
+        ),
+        BBUIGestureRecognizerStateTransition(
+            fromState: .Changed,
+            toState: .Changed,
+            shouldNotify: true
+        ),
+        BBUIGestureRecognizerStateTransition(
+            fromState: .Changed,
+            toState: .Cancelled,
+            shouldNotify: true
+        ),
+        BBUIGestureRecognizerStateTransition(
+            fromState: .Changed,
+            toState: .Ended,
+            shouldNotify: true
+        )
+    ]
+    
+    /**
+        Determines if a transition from one `BBUIGestureRecognizerState` to 
+        another is allowed (is contained in `allowedStateTransitions`).
+    
+        :param: fromState The beginning state of the transition.
+        :param: toState The ending state of the transition.
+    
+        :returns: Returns the allowed `BBUIGestureRecognizerStateTransition` if 
+            it is present in `allowedStateTransitions`, otherwise it returns
+            `nil`.
+    */
+    private func findAllowedTransition(fromState: BBUIGestureRecognizerState, toState: BBUIGestureRecognizerState) -> BBUIGestureRecognizerStateTransition? {
+        for transition in allowedStateTransitions {
+            if transition.fromState == fromState && transition.toState == toState {
+                return transition
+            }
+        }
+        
+        return nil
+    }
+    
+    /// Private storage for the `state` property.
+    private var _state: BBUIGestureRecognizerState = .Possible
+    
+    /**
+        The current `BBUIGestureRecognizerState` of the gesture recognizer. When 
+        `state` transitions from `.Possible` to `.Began` or `.Recognized` and 
+        the `delegate` (if set) prevents the gesture recognizer from beginning, 
+        `state` is automatically set to `.Failed`. Otherwise, `state` is only 
+        updated if the transition is allowed (see `allowedStateTransitions`). 
+        Lastly, if the allowed transition (if found) has `shouldNotify` set to 
+        `true`, the gesture recognizer's associated action method is called.
+    */
+    internal(set) var state: BBUIGestureRecognizerState {
+        get {
+            return _state
+        }
+        set(newState) {
+            
+            var varNewState = newState
+            
+            // If there is a delegate and it says that this recognizer should
+            // not begin, set the new state to .Failed
+            var shouldBegin = true
+            if let actualDelegate = delegate {
+                if _state == .Possible {
+                    if newState == .Began || newState == .Recognized {
+                        shouldBegin = actualDelegate.gestureRecognizerShouldBegin(self)
+                    }
+                }
+            }
+            
+            if !shouldBegin {
+                varNewState = .Failed
+            }
+            
+            if let allowedTransition = findAllowedTransition(_state, toState: varNewState) {
+                _state = varNewState
+                
+                // docs mention that the action messages are sent on the next run loop, so we'll do that here.
+                // note that this means that reset can't happen until the next run loop, either otherwise
+                // the state property is going to be wrong when the action handler looks at it, so as a result
+                // I'm also delaying the reset call (if necessary) below in -continueTrackingWithEvent:
+                if allowedTransition.shouldNotify {
+                    registeredAction?.performAction(self)
+                }
+            } else {
+                println("Invalid state transition from \(_state) to \(varNewState)")
+            }
+        }
+    }
+    
 //    var cancelsTouchesInView = true
 //    var delaysTouchesBegan = false
 //    var delaysTouchesEnded = true
+    
     var enabled = true
     
     var delegate: BBUIGestureRecognizerDelegate? = nil
@@ -66,6 +242,16 @@ class BBUIGestureRecognizer: Equatable, Printable {
     private var registeredAction: BBUIGestureRecognizerTargetAction?
     private var trackingTouches = [UITouch]()
     
+    /**
+        Initializes an allocated gesture-recognizer object with a target and an 
+        action selector.
+    
+        :param: target An object on which a method will be called when this 
+            gesture recognizer recognizes a gesture. `nil` is not a valid value.
+        :param: action A method implemented by the target to handle the gesture 
+            recognized by the receiver. The method must conform to the signature 
+            described in the class overview. `nil` is not a valid value.
+    */
     init<T: AnyObject>(target: T, action: (T) -> (BBUIGestureRecognizer?) -> ()) {
         addTarget(target, action: action)
     }
@@ -73,6 +259,7 @@ class BBUIGestureRecognizer: Equatable, Printable {
     func addTarget<T: AnyObject>(target: T, action: (T) -> (BBUIGestureRecognizer?) -> ()) {
         let targetAction = BBUIGestureRecognizerTargetActionWrapper(target: target, action: action)
         registeredAction = targetAction
+        let a = UIGestureRecognizer()
     }
     
     func removeTarget() {
@@ -80,7 +267,7 @@ class BBUIGestureRecognizer: Equatable, Printable {
     }
     
     func reset() {
-        state = .BBUIGestureRecognizerStatePossible
+        state = .Possible
     }
     
     func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
@@ -175,9 +362,9 @@ class BBUIGestureRecognizer: Equatable, Printable {
         }
         
         switch state {
-        case .BBUIGestureRecognizerStatePossible,
-             .BBUIGestureRecognizerStateBegan,
-             .BBUIGestureRecognizerStateChanged:
+        case .Possible,
+             .Began,
+             .Changed:
             
             if began.count > 0 {
                 touchesBegan(began, withEvent: event)
@@ -231,25 +418,42 @@ class BBUIGestureRecognizer: Equatable, Printable {
         var stateString: String
         
         switch (state) {
-        case .BBUIGestureRecognizerStatePossible:
+        case .Possible:
             stateString = "Possible"
-        case .BBUIGestureRecognizerStateBegan:
+        case .Began:
             stateString = "Began"
-        case .BBUIGestureRecognizerStateChanged:
+        case .Changed:
             stateString = "Changed"
-        case .BBUIGestureRecognizerStateEnded:
+        case .Ended:
             stateString = "Ended"
-        case .BBUIGestureRecognizerStateCancelled:
+        case .Cancelled:
             stateString = "Cancelled"
-        case .BBUIGestureRecognizerStateFailed:
+        case .Failed:
             stateString = "Failed"
-        case .BBUIGestureRecognizerStateRecognized:
+        case .Recognized:
             stateString = "Recognized"
         }
         
         let thisType = ObjectIdentifier(self)
         return "<BBUIGestureRecognizer; state = \(stateString); node = \(node)>"
     }
+    
+//- (void)requireGestureRecognizerToFail:(UIGestureRecognizer *)otherGestureRecognizer
+//{
+//}
+//- (BOOL)canPreventGestureRecognizer:(UIGestureRecognizer *)preventedGestureRecognizer
+//{
+//return YES;
+//}
+//
+//- (BOOL)canBePreventedByGestureRecognizer:(UIGestureRecognizer *)preventingGestureRecognizer
+//{
+//return YES;
+//}
+//
+//- (void)ignoreTouch:(UITouch *)touch forEvent:(UIEvent*)event
+//{
+//}
 }
 
 func ==(lhs: BBUIGestureRecognizer, rhs: BBUIGestureRecognizer) -> Bool {
